2c230bd02dee1a706d2513bd8f37af0024760e62
pooya-tolideh <pooya.tolideh@gmail.com>
2018-07-30 13:52:51 -0400

    created Header component

--- src/app.js
+++ src/app.js
@@ -4,19 +4,37 @@ import ReactDOM from 'react-dom';
 
 
 
+const Header = (props) => {
+    return (
+        <header>
+            <h1>{props.title}</h1>
+            <h2>{props.subtitle}</h2>
+        </header>
+    );
+};
+
+Header.defaultProps = {
+    title: 'Method Weather Network',
+    subtitle: 'Get the latest weather info'
+};
+
+
 class SearchForm extends React.Component {
     render() {
         return <form>
-            <input type="text"/>
+            <input type="text" id="search-city"/>
         </form>
     }
 }
 
 class WeatherApp extends React.Component {
     render() {
-        return (<div>
-            <SearchForm/>
-        </div>);
+        return (
+            <div>
+                <Header/>
+                <SearchForm/>
+            </div>
+        );
     }
 }
 
