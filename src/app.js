e8f4963f5ed97b04d899c5effcd297430ea332a6
pooya-tolideh <pooya.tolideh@gmail.com>
2018-07-30 13:45:52 -0400

    added SearchForm component

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class WeatherApp extends React.Component {
+
+
+
+class SearchForm extends React.Component {
     render() {
-        return <div>weather</div>
+        return <form>
+            <input type="text"/>
+        </form>
     }
 }
 
+class WeatherApp extends React.Component {
+    render() {
+        return (<div>
+            <SearchForm/>
+        </div>);
+    }
+}
 
 ReactDOM.render(<WeatherApp/>, document.getElementById('weather-app'));
+
