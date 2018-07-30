23776381a050dab7db7689f25274503cfddca855
pooya-tolideh <pooya.tolideh@gmail.com>
2018-07-30 13:41:52 -0400

    created the basic structure of the app

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class WeatherApp extends React.Component {
+    render() {
+        return <div>weather</div>
+    }
+}
+
+
+ReactDOM.render(<WeatherApp/>, document.getElementById('weather-app'));
